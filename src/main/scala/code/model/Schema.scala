3a0c15e9d4d1ee591d4dc62cfd0f84e52358e1d7
Richard Dallaway <richard@dallaway.com>
2013-01-13 16:34:53 +0000

    Object reference and embedding example.

--- src/main/scala/code/model/Schema.scala
+++ src/main/scala/code/model/Schema.scala
@@ -3,15 +3,45 @@ package model
 
 import net.liftweb.mongodb.record._
 import net.liftweb.mongodb.record.field._
+import net.liftweb.record.field.{IntField,StringField}
 
 class Country private () extends MongoRecord[Country] with StringPk[Country] {
+
   override def meta = Country
+
   object population extends MongoMapField[Country,Int](this) {
    // override def optional_? = true
   }
+
+  object flag extends BsonRecordField(this, Image)
+
+  object planet extends StringRefField(this, Planet, 128)
+
 }
 
 object Country extends Country with MongoMetaRecord[Country] {
   override def collectionName = "example.earth"
  // override def mongoIdentifier = bootstrap.liftweb.OtherMongoIdentifier
+}
+
+
+
+class Image private () extends BsonRecord[Image] {
+  def meta = Image
+  object url extends StringField(this, 1024)
+  object width extends IntField(this)
+  object height extends IntField(this)
+}
+
+object Image extends Image with BsonMetaRecord[Image]
+
+
+
+class Planet private() extends MongoRecord[Planet] with StringPk[Planet] {
+  override def meta = Planet
+  object review extends StringField(this,1024)
+}
+
+object Planet extends Planet with MongoMetaRecord[Planet] {
+  override def collectionName = "example.planet"
 }
--- src/main/scala/code/snippet/Places.scala
+++ src/main/scala/code/snippet/Places.scala
@@ -7,14 +7,21 @@ import net.liftweb.util.Helpers._
 
 class Places {
 
-  val uk = Country.find("uk") openOr {
+
+
+  lazy val uk = Country.find("uk") openOr {
 
     val info = Map(
       "Brighton" -> 134293,
       "Birmingham" -> 970892,
       "Liverpool" -> 469017)
 
-    Country.createRecord.id("uk").population(info).save
+    val unionJack =
+      Image.createRecord.url("http://bit.ly/unionflag200").width(200).height(100)
+
+    val earth = Planet.createRecord.id("earth").review("Harmless").save
+
+    Country.createRecord.id("uk").population(info).planet(earth.id.is).flag(unionJack).save
   }
 
 
@@ -24,7 +31,11 @@ class Places {
 
     "#facts" #> (
     for { (name,pop) <- uk.population.is } yield
-        ".name *" #> name & ".pop *" #> pop
+        ".name *" #> name &
+          ".pop *" #> pop &
+          ".planet" #> uk.planet.obj.map { p =>
+            ".planet-name *" #> p.id &
+              ".planet-review *" #> p.review }
       )
   }
 
